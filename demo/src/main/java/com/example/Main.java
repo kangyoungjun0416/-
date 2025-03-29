7cb9242c6d7c4f40b0324b6002f77799daee3a6e
kangyoungjun <kangyoungjun@g.yju.ac.kr>
2025-03-30 02:52:25 +0900

    update sibar
    asdqwe

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String message = "let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
